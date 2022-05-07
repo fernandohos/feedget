10e0434a8ba68867fc63cead5beee5fac1e57332
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2022-05-06 23:47:18 -0300

    fix: cannot find module Widget

--- web/src/components/widget.tsx
+++ web/src/components/widget.tsx
@@ -3,7 +3,6 @@ import { ChatTeardropDots } from 'phosphor-react'
 import { WidgetForm } from './WidgetForm'
 
 export function Widget() {
-
     return (
         <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
             <Popover.Panel><WidgetForm /></Popover.Panel>
